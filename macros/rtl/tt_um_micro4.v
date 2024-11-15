52c484c8d2ffd48dfc4e93b6aa730eef250b3e96
Devin <devinatkin@gmail.com>
2024-11-15 07:21:48 -0700

    rename cells?

--- macros/rtl/tt_um_micro4.v
+++ macros/rtl/tt_um_micro4.v
@@ -12,10 +12,5 @@ module tt_um_micro4 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + ui_in;  // Example: ou_out is the sum of ui_in and uio_in
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{clk, rst_n, 1'b0};
 
 endmodule
